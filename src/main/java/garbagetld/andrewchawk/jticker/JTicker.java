b529425320855230c898b3b6121ed3d4476f85d3
andrewchawk <noreply@spam.bin>
2024-04-09 13:22:41 -0500

    I added a crappy placeholder.

---
+++ src/main/java/garbagetld/andrewchawk/jticker/JTicker.java
@@ -0,0 +1,11 @@
+public class
+JTicker {
+	public static void
+	main(String[] args) {
+		throw new RuntimeException(
+			"""
+			JTicker is still completely useless.  Stay tuned!
+			"""
+		);
+	}
+}
