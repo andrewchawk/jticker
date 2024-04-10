75cd795e7778126e1faa04a9b835bc49e63aef9f
andrewchawk <noreply@spam.bin>
2024-04-09 22:28:28 -0500

    TickerOptions now actually has an option!

--- src/main/java/garbagetld/andrewchawk/jticker/TickerOptions.java
+++ src/main/java/garbagetld/andrewchawk/jticker/TickerOptions.java
@@ -6,4 +6,15 @@ package garbagetld.ach.jticker;
  *
  */
 public class TickerOptions {
+	/**
+	 *
+	 * If this value is nonnegative, then by default, each
+	 * new {@code TickerSource} object can have at most
+	 * {@code defaultMaxEntries} articles in the list of ticker entries.
+	 * If this value <i>is</i> negative, then by default, each new
+	 * {@code TickerSource} object can have infinitely many articles in
+	 * the list of ticker entries.
+	 *
+	 */
+	public int defaultMaxEntries;
 }
